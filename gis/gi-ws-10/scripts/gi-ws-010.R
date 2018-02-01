4ac12c79414792214574d5553ec3f3f7fa1269c5
gisma <gisma@gis-ma.org>
2018-02-01 18:04:38 +0100

    add comments control script

--- gis/gi-ws-10/scripts/gi-ws-010.R
+++ gis/gi-ws-10/scripts/gi-ws-010.R
@@ -70,114 +70,97 @@ makGlobalVar("path_run",gi_run)
 makGlobalVar("path_tmp",gi_run)
 makGlobalVar("path_output",gi_output)
 
-
-# rgb 
-#rgb<-raster::stack("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/geonode-_476000_5630000_1.tif")
+# delete run dir
+unlink(paste0(path_run,"*"), force = TRUE)
+# load data
 load("/home/creu/lehre/msc/active/msc-2017/data/gis/output/0_5_10_15_20_50stat.RData")
-
-plot2<-raster::shapefile("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/plot2.shp")
-plot_utm <- sp::spTransform(plot2,CRS(proj4))
+plot2<-raster::shapefile("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/plot_UTM.shp")
 tree3<-raster::shapefile("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/crowns.shp")
-tree3_utm <- sp::spTransform(tree3,CRS(proj4))
-
-
+# assign chm
 chm<-statLayer[[2]][[3]]
-
-
+# assign extent
 ext<- raster::extent(477393.,477460. ,5631938. , 5632003.)
 # link GDAL and SAGA
-# gdal <- link2GI::linkgdalUtils()
+gdal <- link2GI::linkgdalUtils()
 saga <- link2GI::linkSAGA()
 
 # ---------- start pr-processing ---------------------------------------------------  ---------------------------------------------------
 
-cat("::: run pre processing...\n")
+cat("\n::: run pre processing...\n")
 chmR<-raster::crop(chm,ext)
 raster::writeRaster(chmR,paste0(gi_run,"chm.tif"),
                     overwrite = TRUE)
-
-#indices<-c("HI", "GLI",  "GRVI")
-indices <- c("Roughness")
-
+# index for segmentation default is chm
+indices <- c("chm")
 for (item in indices){
-  gdaldem(item,paste0(gi_run,"chm.tif"),paste0(gi_run,item,".tif"))
-}
-# (GDAL) convert the TIF to SAGA format
-for (index in indices) {
-  cat("convert ",index,"\n")
-  gdalUtils::gdalwarp(paste0(gi_run,item,".tif"), 
-                      paste0(gi_run,item,".sdat"), 
-                      overwrite=TRUE,  
-                      of='SAGA') 
+  gdalUtils::gdalwarp(paste0(path_run,item,".tif"), 
+                      paste0(path_run,item,".sdat"), 
+                      overwrite = TRUE,  
+                      of = 'SAGA',
+                      verbose = FALSE)
 }
 
-# ----- start ADDON RGB image processing ---------------------------------------------------
-# rgb <- raster::crop(rgb,ext)
-# rgb <- raster::resample(rgb, chmR, method = 'bilinear')
-# raster::writeRaster(rgb,paste0(path_output,"ortho.tif"),
-#                     overwrite = TRUE)
-# #cat("::: calculate RGBI... \n")
-# rgbI <- uavRst::rgbIndices(rgb[[1]],rgb[[2]],rgb[[3]],indices)
 
-# cat("\n convert indices to SAGA... \n")
-# i <- 1
-# for (index in indices) {
-#   cat("convert ",index,"\n")
-#   uavRst::r2saga(rgbI[[i]],index)
-#   i <- i + 1
-# }
-# 
 # ----  start crown analysis --------------------------------------------------------
 
+# call seeding process
+seeds <- uavRst::fa_treeSeeding(chmR,
+                                minTreeAlt = 12.5,
+                                crownMinArea = 3,
+                                crownMaxArea = 225,
+                                is0_join = 1, 
+                                is0_thresh = 0.20, 
+                                seeding = TRUE
+)
+
 # call tree crown segmentation 
-crowns <- uavRst::fa_crown_segmentation(chmR,
-                                        minTreeAlt = 15,
-                                        crownMinArea = 5,
-                                        is0_join = 1, 
-                                        is0_thresh = 0.3, 
-                                        majority_radius = 2.0, 
-                                        is3_sig1 = 0.010,
-                                        is3_leafsize = 256, 
-                                        is3_neighbour = 0,
-                                        is3_sig2 = 2.5,
-                                        is3_threshold = 0.0005,
+crowns <- uavRst::fa_crown_segmentation(seeds = seeds,
+                                        majority_radius = 9.0,
+                                        is3_sig1 = 0.05,
+                                        is3_sig2 = 0.05,
+                                        is3_threshold = 0.00005,
                                         is3_seed_params = indices,
                                         seeding = TRUE
 )
 
-# calculate potential insolation
-#uavRst::potInsolation("chm",pi_day = "01/06/2017",pi_day_stop = "30/06/2017",pi_hour_step = 1.0)
-
 cat("::: run post-classification...\n")
 # extract stats
 polyStat <- uavRst::xpolystat(c("chm"),
-                              spdf = "tree_crowns.shp")
+                              spdf = crowns)
 
 # calculate metrics of the crown geometries
 crowns <- uavRst::fa_caMetrics(polyStat)
 
-# export shapefile
+# export geojson
 sf::st_write(sf::st_as_sf(crowns), "crowns.geojson",delete_dsn=TRUE,driver="GeoJSON")
-# simple filtering of crownareas based on tree height min max area and artifacts at the analysis/image borderline
-trees_crowns <- uavRst::fa_basicTreeCrownFilter(crownFn = paste0(path_run,"crowns.shp"),
-                                                minTreeAlt = 8,
-                                                crownMinArea = 5,
-                                                crownMaxArea = 200,
-                                                opt = 0.5,
-                                                TAchmQ = "chmQ50",
-                                                TAopt = "solidity")
-# extract all pixvalues according to a vector geometry
-# pixvalues <- basicExtraction(x = chmR,fN = trees_crowns_2[[2]],responseCat = "ID")
-
-# cut result is with reference
-finalTrees<-rgeos::gIntersection(trees_crowns[[2]],plot2,byid = TRUE)
-mapview::mapview(finalTrees)+chmR+tree3+chmR
-
-# save results to shape
-rgdal::writeOGR(obj = crowns, 
+rgdal::writeOGR(obj = crowns,
                 layer = "crowns", 
                 driver = "ESRI Shapefile", 
                 dsn = path_run, 
                 overwrite_layer = TRUE)
+# simple filtering of crownareas based on tree height min max area and artifacts at the analysis/image borderline
+trees_crowns <- uavRst::fa_basicTreeCrownFilter(crownFn = paste0(path_run,"crowns.geojson"),
+                                                minTreeAlt = 5,
+                                                crownMinArea = 5,
+                                                crownMaxArea = 150,
+                                                mintreeAltParam = "chmQ20"
+)
+
+# vie it
+pl<-mapview(plot2)
+tc<-mapview(trees_crowns[[2]])
+tc+p
+
+# cut result is with reference
+finalTrees<-rgeos::gIntersection(plot2,trees_crowns[[2]],,byid = TRUE,)
+plot(finalTrees)
+
+
+# calculate potential insolation
+#uavRst::potInsolation("chm",pi_day = "01/06/2017",pi_day_stop = "30/06/2017",pi_hour_step = 1.0)
+
+# extract all pixvalues according to a vector geometry
+# pixvalues <- basicExtraction(x = chmR,fN = trees_crowns_2[[2]],responseCat = "ID")
+
 
 cat(":: ...finsihed \n")
